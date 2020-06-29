b130348234713dc2a8396408b431f796f5a10f46
Rajendran <Duraimurugan.X.Rajendran.-ND@disney.com>
2020-06-28 20:01:10 -0400

    in operator usage

--- index.ts
+++ index.ts
@@ -6,3 +6,34 @@ function greet({greeting, name}: Salutation): string {
 }
 
 console.log(greet({greeting: 'Hello', name: 'Durai'}));
+
+
+
+interface Admin {
+    id: string,
+    role: string
+}
+
+interface User {
+    id: string,
+    email: string
+}
+
+/**
+ *@param userRole : Admin or User 
+ * we can use typescript 'in' operator to check if the property exists on the object
+ * and to hint the tsc on the type of the object
+ */
+
+function redirectByUserRole(userRole: Admin | User) {
+    if ('role' in userRole) {
+
+        console.log('Admin: ' + userRole.role);
+        // redirect logic for admin
+    } else {
+        console.log('User');
+        // redirect logic for user
+    }
+}
+
+redirectByUserRole({id: '1', role: 'SuperAdmin'});
